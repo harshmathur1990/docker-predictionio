7641f3f098a48db34e4b4e8cfd5361cd8262d9e5
Harsh Mathur <harshmathur.1990@gmail.com>
2016-10-25 14:07:05 +0530

    changes for juggernaut

--- quickstartapp/run.sh
+++ quickstartapp/run.sh
@@ -19,8 +19,8 @@ echo "Step 1: Passed"
 
 echo "Step 2. Create a new Engine from an Engine Template"
 
-echo "n" | pio template get apache/incubator-predictionio-template-recommender MyRecommendation --name "none" --package "none" --email "none"
-cd MyRecommendation
+echo "n" | pio template get harshmathur1990/template-scala-parallel-universal-recommendation JuggernautRecommendor --name "none" --package "none" --email "none"
+cd JuggernautRecommendor
 
 echo "Step 2: Passed"
 
@@ -28,7 +28,7 @@ echo "Step 2: Passed"
 echo "Step 3. Generate an App ID and Access Key"
 
 #echo "YES" | pio app delete MyApp1
-pio app new MyApp1 > log.txt
+pio app new JuggernautRecommendor > log.txt
 KEY=$(grep "Access Key:" log.txt | awk '{print $5}')
 echo "KEY=$KEY"
 
@@ -37,12 +37,12 @@ echo "Step 3: Passed"
 
 echo "Step 4 Import Sample Data"
 
-curl https://raw.githubusercontent.com/apache/spark/master/data/mllib/sample_movielens_data.txt --create-dirs -o data/sample_movielens_data.txt
-python data/import_eventserver.py --access_key $KEY
+# curl https://raw.githubusercontent.com/apache/spark/master/data/mllib/sample_movielens_data.txt --create-dirs -o data/sample_movielens_data.txt
+# python data/import_eventserver.py --access_key $KEY
 
 
 echo "Step 5. Deploy the Engine as a Service"
-sed -i "s|INVALID_APP_NAME|MyApp1|" /quickstartapp/MyRecommendation/engine.json
+sed -i "s|INVALID_APP_NAME|MyApp1|" /quickstartapp/JuggernautRecommendor/engine.json
 
 echo "Building...  It may take some time to download all the libraries."
 pio build --verbose
@@ -50,4 +50,4 @@ pio build --verbose
 echo "Taining..."
 pio train -- --driver-memory 4G
 
-echo "You may now deploy engine by running cd /quickstartapp/MyRecommendation && pio deploy --ip 0.0.0.0"
+echo "You may now deploy engine by running cd /quickstartapp/JuggernautRecommendor && pio deploy --ip 0.0.0.0"
